002fd96066db4bf90e911141ff713f7a3a33d969
rubaiyat11 <rubaiyatabdullahrabi71@gmail.com>
2026-05-02 21:18:49 +0600

    Better MPC with constraint Calculations

--- MPC_intro/mpc_sim.py
+++ MPC_intro/mpc_sim.py
@@ -26,14 +26,12 @@ Q = np.diag([20, 20, 5, 5])
 R = np.diag([0.2, 0.2])
 Rd = np.diag([0.5, 0.5])
 
-N = 4
+N = 5
 D = 0.95
 
-u_candidates = [
-    np.array([ax, ay])
-    for ax in [-1, 0, 1]
-    for ay in [-1, 0, 1]
-]
+num_samples = 300
+
+mean_sequence = [np.zeros(2) for _ in range(N)]
 
 u_prev = np.array([0.0, 0.0])
 
@@ -48,15 +46,32 @@ def simulate(x, u_sequence):
         error = x_sim - target
         pos_error = x_sim[:2] - target[:2]
         vel = x_sim[2:]
+        u = np.clip(u, -2, 2)
+        
         du = u - prev_u
-        direction_cost = np.dot(pos_error, vel)
+        direction_cost = np.dot(pos_error, vel)        
 
         cost = error.T @ Q @ error + u.T @ R @ u + du.T @ Rd @ du + 0.5 * direction_cost
         total_cost += (gamma ** t) * cost
 
+        u_limit = 2.0
+        v_limit = 5.0
+
+        vel_violation = np.maximum(0, np.abs(x_sim[2:]) - v_limit)
+        total_cost += 500 * np.sum(vel_violation**2)
+
+        control_violation = np.maximum(0, np.abs(u) - u_limit)
+        total_cost += 500 * np.sum(control_violation**2)
+
+        x_limit = 20.0
+        y_limit = 20.0
+
         x_sim = A @ x_sim + B @ u
 
         x_sim[2:]  *= D
+
+        if abs(x_sim[0]) > x_limit or abs(x_sim[1]) > y_limit:
+            total_cost += 3000
 
         prev_u = u
 
@@ -67,20 +82,43 @@ def simulate(x, u_sequence):
     return total_cost
     
 
+def compute_cost(u_sequence):
+    return simulate(x, u_sequence)
+
+
+def compute_gradient(u_sequence):
+    grad = np.zeros_like(u_sequence)
+    eps = 1e-3
+
+    base_cost = compute_cost(u_sequence)
+
+    for t in range(N):
+        for i in range(2):
+            u_perturbed = u_sequence.copy()
+            u_perturbed[t, i] += eps
+
+            cost = compute_cost(u_perturbed)
+
+            grad[t, i] = (cost - base_cost) / eps
+
+    return grad
+
+
 for step in range(100):
-    best_cost = float("inf")
-    best_u = None
 
-    for u_sequence in product(u_candidates, repeat=N):
+    # initial guess
+    u_sequence = np.zeros((N, 2))
 
-        cost = simulate(x, u_sequence)
+    # gradient descent iterations
+    for _ in range(10):
+        grad = compute_gradient(u_sequence)
 
-        if(cost < best_cost):
-            best_cost = cost
-            best_u = u_sequence[0]
+        u_sequence = u_sequence - 0.1 * grad
+        u_sequence = np.clip(u_sequence, -2, 2)
+
+    best_u = u_sequence[0]
 
     x = A @ x + B @ best_u
-
     x[2:] *= D
 
     u_prev = best_u
