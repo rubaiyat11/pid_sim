521d2ef7d9b79240fc39564bd99ad3413c8e84b7
rubaiyat11 <rubaiyatabdullahrabi71@gmail.com>
2026-05-04 23:22:24 +0600

    Fixed linearity issue in LQR

--- LQR_intro/lqr_sim.py
+++ LQR_intro/lqr_sim.py
@@ -4,9 +4,6 @@ from scipy.linalg import solve_continuous_are
 from collections import deque
 
 
-#BROKE THE CONTROLLER THANKS TO EXCESS DAMPING & NON-LINEARITY
-
-
 dt = 0.01
 
 x = np.array([0.0, 0.0, 0.0, 0.0])
@@ -27,40 +24,22 @@ B= np.array([
     [0, 1/m]
 ])
 
-Q = np.diag([10, 10, 1, 1])
-R = np.diag([0.1, 0.1])
+Q = np.diag([10, 10, 2, 2])
+R = np.diag([0.5, 0.5])
 
 P = solve_continuous_are(A, B, Q, R)
 
-K = np.linalg.inv(R) @ B.T @ P
-
-u_buffer = deque([np.zeros(2)] * 10, maxlen=10)
+K = np.linalg.inv(R) @ B.T @ P    
 
 for i in range(1000):
-    target[0] += 0.01
-    target[1] += 0.005
 
     error = x - target
 
     u = -K @ error
     u = np.clip(u, -2.0, 2.0)
 
-    u_buffer.append(u)
-    u_applied = u_buffer[0]
-
-    c = 0.5
-    A[2, 2] = -c/m
-    A[3, 3] = -c/m
-
-    disturbance = np.array([0.0, 0.0, 0.2, -0.1])
-
-    x_dot = A @ x + B @ u_applied + disturbance
-    
-    drag = 0.05
-    x_dot[2] -= drag * x[2] * abs(x[2])
-    x_dot[3] -= drag * x[3] * abs(x[3])
+    x_dot = A @ x + B @ u
 
     x += x_dot * dt
-
 
     print(x[:2])
